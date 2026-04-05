925a6090ba5b196626609aa7d5a98987e8b6f6b3
Mayank Kaushik <mayank.kaushik@grappus.com>
2026-04-05 06:21:50 +0000

    delete

--- src/routes/_app.inventory.products.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute, Outlet } from '@tanstack/react-router'
-
-export const Route = createFileRoute('/_app/inventory/products')({
-  component: ProductsLayout,
-})
-
-function ProductsLayout() {
-  return <Outlet />
-}
